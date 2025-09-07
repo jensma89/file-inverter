c9f99ee3c9d80ea28eaccc5de6cee5f47e9e90da
Jens M. <jensmayer89@gmail.com>
2025-09-07 19:56:09 +0200

    Installing libraries

--- ui_fileinverter.py
+++ ui_fileinverter.py
@@ -3,4 +3,9 @@ ui_inverter.py
 
 The GUI of the file inverter.
 """
-
+import tkinter as tk
+import tkinter.font as tkfont
+from tkinter import filedialog, messagebox, ttk
+from pathlib import Path
+from datetime import datetime
+import shutil
